b28766778213d4ce8af0bec9eee03ddc6925817d
allthesignals <wilburnforce@gmail.com>
2017-04-26 14:05:35 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :schools
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -6,6 +7,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+
+  resources :districts
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
