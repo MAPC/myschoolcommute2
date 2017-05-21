b60fd8c22ca9e8205e55cc9d3ddcae90f0dd17fd
allthesignals <wilburnforce@gmail.com>
2017-05-21 19:07:33 -0400

    Begin rails form logic for submitting a survey.
    Must add survey responses to routes so that it creates the survey_responses_path helper.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :survey_responses
   resources :schools
   get 'welcome/index'
 
   resources :surveys, only: [:show, :index]
+  resources :survey_responses, only: [:post]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
