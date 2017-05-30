299b2fdeb52575eef64d8d9704dce3a281bbf2ff
allthesignals <wilburnforce@gmail.com>
2017-05-30 12:33:12 -0400

    Authorize controllers
    Admin view requires an “is_admin” on the user. Other views only need either “is_admin” or “is_district” flagged true.

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -10,12 +10,12 @@ module Admin
     before_action :authenticate_admin
 
   def authenticate_admin
-    redirect_to '/', alert: 'Not authorized.' unless current_user
+    redirect_to '/', alert: 'Not authorized - please notify program coordinator to grant access.' unless current_user && access_whitelist
   end
 
   private
     def access_whitelist
-      current_user.try(:admin?) || current_user.try(:door_super?)
+      current_user.try(:is_admin?)
     end
 
     # Override this value to specify the number of elements to display at a time
