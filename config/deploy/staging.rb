47005dde2fb2b650343cf48fad669228c74ab10f
Matthew Zagaja <mzagaja@alum.wpi.edu>
2017-06-05 10:18:25 -0400

    Remove unused properties

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '174.129.122.98', user: 'myschoolcommute2', roles: %w{app db web}, my_property: :my_value
+server '174.129.122.98', user: 'myschoolcommute2', roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
