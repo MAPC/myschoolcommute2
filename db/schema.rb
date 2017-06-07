3ee4bcb8dfdef1e303720f77fec0228b4e5336ec
allthesignals <wilburnforce@gmail.com>
2017-06-07 16:02:36 -0400

    Migration for melted view of surveys
    This adds a migration that creates a SQL view which “melts” or pivots the survey_responses table in a way that the legacy R script can understand. Adds a new gem, “scenic”

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606203348) do
+ActiveRecord::Schema.define(version: 20170607193223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -222,4 +222,37 @@ ActiveRecord::Schema.define(version: 20170606203348) do
   end
 
   add_foreign_key "surveys", "schools"
+
+  create_view "melted_survey_responses",  sql_definition: <<-SQL
+      SELECT melted.id,
+      melted.survey_id,
+      melted.distance,
+      melted.geometry,
+      melted.created,
+      melted.modified,
+      melted.shed,
+      melted.dropoff,
+      melted.from_school,
+      melted.grade,
+      melted.pickup,
+      melted.to_school
+     FROM ( SELECT survey_responses.id,
+              survey_responses.survey_id,
+              survey_responses.distance,
+              survey_responses.geometry,
+              survey_responses.created_at AS created,
+              survey_responses.updated_at AS modified,
+              survey_responses.shed,
+              survey_responses.nr_licenses,
+              survey_responses.nr_vehicles,
+              unnest(ARRAY[survey_responses.dropoff_0, survey_responses.dropoff_1, survey_responses.dropoff_10, survey_responses.dropoff_11, survey_responses.dropoff_12, survey_responses.dropoff_13, survey_responses.dropoff_14, survey_responses.dropoff_15, survey_responses.dropoff_16, survey_responses.dropoff_17, survey_responses.dropoff_18, survey_responses.dropoff_19, survey_responses.dropoff_2, survey_responses.dropoff_3, survey_responses.dropoff_4, survey_responses.dropoff_5, survey_responses.dropoff_6, survey_responses.dropoff_7, survey_responses.dropoff_8, survey_responses.dropoff_9]) AS dropoff,
+              unnest(ARRAY[survey_responses.from_school_0, survey_responses.from_school_1, survey_responses.from_school_10, survey_responses.from_school_11, survey_responses.from_school_12, survey_responses.from_school_13, survey_responses.from_school_14, survey_responses.from_school_15, survey_responses.from_school_16, survey_responses.from_school_17, survey_responses.from_school_18, survey_responses.from_school_19, survey_responses.from_school_2, survey_responses.from_school_3, survey_responses.from_school_4, survey_responses.from_school_5, survey_responses.from_school_6, survey_responses.from_school_7, survey_responses.from_school_8, survey_responses.from_school_9]) AS from_school,
+              unnest(ARRAY[survey_responses.grade_0, survey_responses.grade_1, survey_responses.grade_10, survey_responses.grade_11, survey_responses.grade_12, survey_responses.grade_13, survey_responses.grade_14, survey_responses.grade_15, survey_responses.grade_16, survey_responses.grade_17, survey_responses.grade_18, survey_responses.grade_19, survey_responses.grade_2, survey_responses.grade_3, survey_responses.grade_4, survey_responses.grade_5, survey_responses.grade_6, survey_responses.grade_7, survey_responses.grade_8, survey_responses.grade_9]) AS grade,
+              unnest(ARRAY[survey_responses.pickup_0, survey_responses.pickup_1, survey_responses.pickup_10, survey_responses.pickup_11, survey_responses.pickup_12, survey_responses.pickup_13, survey_responses.pickup_14, survey_responses.pickup_15, survey_responses.pickup_16, survey_responses.pickup_17, survey_responses.pickup_18, survey_responses.pickup_19, survey_responses.pickup_2, survey_responses.pickup_3, survey_responses.pickup_4, survey_responses.pickup_5, survey_responses.pickup_6, survey_responses.pickup_7, survey_responses.pickup_8, survey_responses.pickup_9]) AS pickup,
+              unnest(ARRAY[survey_responses.to_school_0, survey_responses.to_school_1, survey_responses.to_school_10, survey_responses.to_school_11, survey_responses.to_school_12, survey_responses.to_school_13, survey_responses.to_school_14, survey_responses.to_school_15, survey_responses.to_school_16, survey_responses.to_school_17, survey_responses.to_school_18, survey_responses.to_school_19, survey_responses.to_school_2, survey_responses.to_school_3, survey_responses.to_school_4, survey_responses.to_school_5, survey_responses.to_school_6, survey_responses.to_school_7, survey_responses.to_school_8, survey_responses.to_school_9]) AS to_school
+             FROM survey_responses
+            ORDER BY survey_responses.id) melted
+    WHERE ((melted.dropoff IS NOT NULL) AND (melted.from_school IS NOT NULL) AND (melted.grade IS NOT NULL) AND (melted.pickup IS NOT NULL) AND (melted.to_school IS NOT NULL));
+  SQL
+
 end
