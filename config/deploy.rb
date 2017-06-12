548a5dfec893aa39cac6ae6b3b8c86e109b070df
Matthew Zagaja <mzagaja@alum.wpi.edu>
2017-06-12 14:06:12 -0400

    Mitigate disk space problems by keeping 2 releases

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,4 +30,4 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 2
