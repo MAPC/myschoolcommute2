cc7739ee5cab03309b689229993e1da4b9ba186a
Matthew Zagaja <mzagaja@alum.wpi.edu>
2017-06-12 20:15:56 -0400

    Fix db:seed
    Add host for server database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create({ email: 'admin@user.org', password: 'password', is_admin: true })
 
-if Rails.env.development?
+if Rails.env.development? || Rails.env.test?
   sh "pg_restore -a -O -d #{Rails.configuration.database_configuration[Rails.env]['database']} -t schools -t survey_network_bike -t survey_network_walk lib/seeds/mysc-seed.dump"
 else
-  sh "pg_restore -a -O -U #{Rails.configuration.database_configuration[Rails.env]['username']} -d #{Rails.configuration.database_configuration[Rails.env]['database']} -t schools -t survey_network_bike -t survey_network_walk lib/seeds/mysc-seed.dump"
+  sh "pg_restore -a -O -h db.live.mapc.org -U #{Rails.configuration.database_configuration[Rails.env]['username']} -d #{Rails.configuration.database_configuration[Rails.env]['database']} -t schools -t survey_network_bike -t survey_network_walk lib/seeds/mysc-seed.dump"
 end
