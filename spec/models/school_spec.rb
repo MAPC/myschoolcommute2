d2e251eff2d5ddee67fd5069e15cacc2c3f1c5dc
allthesignals <wilburnforce@gmail.com>
2017-06-16 14:49:14 -0400

    Prove that creating new school is possible
    with callbacks. This required splitting out the callback skipper into a factory trait.

--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.describe School, type: :model do
   it 'should return the school latitude in wgs84' do
-    school = create(:school)
+    school = create(:school, :skip_update_sheds)
     expect(school.wgs84_lat.to_s).to match(/\d{2}\.\d{13}/)
+  end
+
+  it 'should allow creation of a school' do
+    school = build(:school)
+    school.save
   end
 end
