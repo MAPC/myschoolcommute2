c6dbb9075e4bd327d03df42c3cc305ba2c2ae29c
allthesignals <wilburnforce@gmail.com>
2017-07-03 15:18:50 -0400

    Remove fonts from precompile

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -42,7 +42,6 @@ Rails.application.configure do
   }
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-  config.assets.precompile << /\.(?:svg|eot|woff|ttf|woff2)\z/
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
