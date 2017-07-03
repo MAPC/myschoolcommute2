08894528651290caa13c875504ce716155a12d80
allthesignals <wilburnforce@gmail.com>
2017-07-03 11:10:01 -0400

    Except new action on survey response dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :districts, only: [:index, :show, :update, :edit]
     resources :schools
     resources :surveys
-    resources :survey_responses
+    resources :survey_responses, except: [:new]
     resources :users
 
     root to: "districts#index"
