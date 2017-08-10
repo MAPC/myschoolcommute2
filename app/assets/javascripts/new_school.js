dfe564c1a1306e7960f42a8d73be00e2a015446c
Matthew Zagaja <mzagaja@alum.wpi.edu>
2017-08-10 14:06:36 -0400

    Resolve #29 Add School Fields
    This commit adds the ability to pick the location of the school and appropriately transforms it from the leaflet SRID to the SRID we use in the database. The school model now assumes that the input SRID is always 4326 and transforms it to the MA mercator projection.

---
+++ app/assets/javascripts/new_school.js
@@ -0,0 +1,24 @@
+ var mymap = L.map('mapid').setView([42.360, -72.058], 9);
+ var clickMarker
+ var latlong
+
+ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+     maxZoom: 18,
+     id: 'mapbox.streets',
+     accessToken: 'pk.eyJ1IjoibXphZ2FqYSIsImEiOiJ5N0Y3Z3pVIn0.Wqu2VeG4ycaw0nPrwy0-sA'
+ }).addTo(mymap);
+
+ function onMapClick(e) {
+   if (clickMarker) {
+    clickMarker.setLatLng([e.latlng.lat, e.latlng.lng])
+    latlong.setAttribute("value", "POINT(" + e.latlng.lng + " " + e.latlng.lat + ")")
+    } else {
+      clickMarker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(mymap);
+      latlong = L.DomUtil.get("school_geometry");
+      latlong.setAttribute("value", "POINT(" + e.latlng.lng + " " + e.latlng.lat + ")")
+   }
+ }
+
+ mymap.on('click', onMapClick);
+ // Potential refactor suggestion link: https://jsfiddle.net/f2hkkx8c/
