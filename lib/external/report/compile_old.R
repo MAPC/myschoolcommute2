7b815198a3c0e4e0997b5e3d287beb3cdcf2abd5
Tarin A. Comer <tcomer@mapc.org>
2022-01-03 15:23:59 -0500

    add compile file with error reporting

---
+++ lib/external/report/compile_old.R
@@ -0,0 +1,38 @@
+library(RPostgreSQL)
+library(DBI)
+library(reshape2)
+library(plyr)
+library(ggplot2)
+library(scales)
+library(knitr)
+library(Hmisc)
+library(httr)
+library(car)
+
+DATE1 = "2012-06-01"
+DATE2 = "2013-06-01"
+setwd('lib/external/report')
+load('.RData')
+
+commandArguments = commandArgs(trailingOnly=TRUE)
+args = unlist(strsplit(commandArguments, split=" ")) #2
+
+dbname = args[1]
+ORG_CODE = args[2]
+DATE1 = args[3]
+DATE2 = args[4]
+SURVEY_ID = args[5]
+MAP_PNG = args[6]
+
+source("generate_report.R")
+# test with 1 response
+# ORG_CODE = "06450310"
+# source("generate_report.R")
+# test with more than 10 responses
+# ORG_CODE = "02480014"
+# source("generate_report.R")
+# test with missing org code
+# ORG_CODE = "05160002"
+# source("generate_report.R")
+
+
